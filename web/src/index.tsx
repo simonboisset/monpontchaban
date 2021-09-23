1e8443fa6183a3d00edcf30bcbb46878e5536267
Simon Boisset <sboisset@ambuliz.com>
2021-09-23 22:19:27 +0200

    Add Sentry for the web

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,12 +1,23 @@
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+import { Loading } from 'components/Loading';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={Loading}>
+      <App />
+    </Sentry.ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
