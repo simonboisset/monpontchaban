681a33cbea3d9766f4375c377682fffc2e60842d
Simon Boisset <sboisset@ambuliz.com>
2021-09-26 23:11:41 +0200

    Auto Increment Version Code with package version

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,8 +1,51 @@
 import 'dotenv/config';
-import app from './app.json';
+import packages from './package.json';
+
+const getAndroidVersionCode = (version) => {
+  let [major, minor, patch] = version.split('.');
+  if (major.length === 1) {
+    major = `0${major}`;
+  }
+  if (minor.length === 1) {
+    minor = `0${minor}`;
+  }
+  if (patch.length === 1) {
+    patch = `0${patch}`;
+  }
+  return Number(`420${major}${minor}${patch}`);
+};
+
+const versionCode = getAndroidVersionCode(packages.version);
 
 export default {
-  ...app.expo,
+  name: 'Mon Pont Chaban',
+  slug: 'mon-pont-chaban',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#84a59d',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    permissions: [],
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#84a59d',
+    },
+    versionCode,
+    package: 'com.simonboisset.monpontchaban',
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
   hooks: {
     postPublish: [
       {
