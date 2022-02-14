112b8778a6873440cfdc551f6b4fc8304c9dedec
Simon Boisset <sboisset@ambuliz.com>
2022-02-15 00:48:23 +0100

    Async storage notification config

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,25 +1,44 @@
 import { fr } from 'const/translation';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
+import Notification from './Notification';
+import NotificationOFF from './NotificationOFF';
 
 const Container = styled.View`
   flex-direction: row;
-  justify-content: center;
   align-items: center;
   height: 72px;
 `;
+
+const FakeIcon = styled.View`
+  width: 16px;
+`;
+
 const Text = styled.Text`
+  flex: 1;
+  text-align: center;
   font-family: ${({ theme }) => theme.typography.h2.font};
   font-size: ${({ theme }) => theme.typography.h2.size}px;
   color: ${({ theme }) => theme.colors.white.main};
 `;
 
-export const Header: React.FC = () => {
+export const Header: React.FC<{ onToggleNotifications: () => void; enableNotifications: boolean }> = ({
+  onToggleNotifications,
+  enableNotifications,
+}) => {
   return (
     <Container>
-      <SafeAreaView edges={['top']}>
+      <SafeAreaView
+        edges={['top']}
+        style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', paddingRight: 24, paddingLeft: 24 }}
+      >
+        <FakeIcon />
         <Text>{fr.MyChaban}</Text>
+        <TouchableOpacity onPress={onToggleNotifications}>
+          {enableNotifications ? <Notification /> : <NotificationOFF />}
+        </TouchableOpacity>
       </SafeAreaView>
     </Container>
   );
