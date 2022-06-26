25156de61bbf03f54cc4db410ec4bb19bde3c30c
Simon Boisset <sboisset@ambuliz.com>
2022-06-26 13:35:24 +0200

    feat: Better server log for action

--- app/app/routes/notification.subscribe.tsx
+++ app/app/routes/notification.subscribe.tsx
@@ -8,6 +8,7 @@ export const action: ActionFunction = async ({ request }) => {
 
   const token = data.token;
   if (!token) {
+    console.error('[Notification Subscribe] Token not found');
     return json({ error: '[Notification Subscribe] Token not found' }, { status: 400 });
   }
 
@@ -17,8 +18,10 @@ export const action: ActionFunction = async ({ request }) => {
     } else {
       await db.device.upsert({ where: { token }, create: { token, active: true }, update: { active: true } });
     }
+    console.info('[Notification Subscribe] Success');
     return json({ success: '[Notification Subscribe] Success' }, { status: 200 });
   } catch (error) {
+    console.error('[Notification Subscribe] Save token failed', error);
     return json({ error: '[Notification Subscribe] Save token failed' }, { status: 400 });
   }
 };
