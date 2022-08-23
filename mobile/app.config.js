40fbb97d923598070a8f72fc3967400cdccfa702
Simon Boisset <sboisset@ambuliz.com>
2022-08-24 00:40:16 +0200

    feat: Setup Expo eas update

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -30,6 +30,7 @@ export default {
   },
   updates: {
     fallbackToCacheTimeout: 0,
+    url: 'https://u.expo.dev/f4033f91-933d-4f91-ae6d-6f8483abf08e',
   },
   assetBundlePatterns: ['**/*'],
   ios: {
