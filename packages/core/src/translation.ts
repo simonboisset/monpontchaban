e117041ac4b67f8369f49bfb86a19396e24fb50d
Simon Boisset <sboisset@ambuliz.com>
2022-09-08 13:52:36 +0200

    Test update

--- packages/core/src/translation.ts
+++ packages/core/src/translation.ts
@@ -1,6 +1,6 @@
 export const fr = {
   loading: 'Chargement ...',
-  MyChaban: 'Horaires du Pont Chaban-Delmas',
+  MyChaban: 'Horaires du Pont Chaban',
   opened: 'Ouvert',
   closeIn: 'Ferme dans',
   reopenIn: 'Réouvre dans',
