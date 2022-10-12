42f96b658372c635ed026cb4beb3c9d798cad1c5
Simon Boisset <simonboisset.dev@gmail.com>
2022-10-12 22:20:51 +0200

    feat: Mobile monitoring

--- apps/mobile/src/const/storage.ts
+++ apps/mobile/src/const/storage.ts
@@ -1,46 +1,75 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import crypto from 'crypto';
+import { chabanMonitor } from 'src/monitor';
 const setItem = async (value: string) => {
   try {
     await AsyncStorage.setItem('enable-notifications', value);
-  } catch (e) {}
+  } catch (e) {
+    chabanMonitor().error('[AsyncStorage] setItem', `${e}`);
+  }
 };
 
 const getItem = async () => {
   try {
     return AsyncStorage.getItem('enable-notifications');
-  } catch (e) {}
+  } catch (e) {
+    chabanMonitor().error('[AsyncStorage] getItem', `${e}`);
+  }
 };
 
 const enableNotification = async () => {
   try {
     return AsyncStorage.setItem('enable-notifications', 'true');
-  } catch (e) {}
+  } catch (e) {
+    chabanMonitor().error('[AsyncStorage] enableNotification', `${e}`);
+  }
 };
 const desableNotification = async () => {
   try {
     return AsyncStorage.setItem('enable-notifications', 'false');
-  } catch (e) {}
+  } catch (e) {
+    chabanMonitor().error('[AsyncStorage] desableNotification', `${e}`);
+  }
 };
 const hasNotification = async () => {
   try {
     const notification = await AsyncStorage.getItem('enable-notifications');
     return notification === 'true';
   } catch (e) {
+    chabanMonitor().error('[AsyncStorage] hasNotification', `${e}`);
     return false;
   }
 };
 const setPushTokenSent = async () => {
   try {
     return AsyncStorage.setItem('push-token-sent', 'true');
-  } catch (e) {}
+  } catch (e) {
+    chabanMonitor().error('[AsyncStorage] setPushTokenSent', `${e}`);
+  }
 };
 const hasPushTokenSent = async () => {
   try {
     const token = await AsyncStorage.getItem('push-token-sent');
     return token === 'true';
-  } catch (e) {}
+  } catch (e) {
+    chabanMonitor().error('[AsyncStorage] getVisitor', `${e}`);
+  }
 };
+
+const getVisitor = async () => {
+  try {
+    const visitor = await AsyncStorage.getItem('visitor');
+    if (visitor) {
+      return visitor;
+    }
+    const newVisitor = crypto.randomUUID();
+    await AsyncStorage.setItem('visitor', newVisitor);
+    return newVisitor;
+  } catch (e) {
+    chabanMonitor().error('[AsyncStorage] getVisitor', `${e}`);
+  }
+};
+
 export const storage = {
   setItem,
   getItem,
@@ -49,4 +78,5 @@ export const storage = {
   hasNotification,
   hasPushTokenSent,
   setPushTokenSent,
+  getVisitor,
 };
