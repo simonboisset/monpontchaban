c16e7992db914a99c1f8d452af61de55e6158710
Simon Boisset <simon@lezo.dev>
2023-04-25 18:52:40 +0200

    fix(mobile): Request Notification permission if is not accepted

--- apps/chaban-mobile/src/services/useToggleSubscribeWithoutAuth.ts
+++ apps/chaban-mobile/src/services/useToggleSubscribeWithoutAuth.ts
@@ -1,6 +1,8 @@
 import { lezoAlertApi } from '@lezo-alert/sdk';
 import { useQueryClient } from '@tanstack/react-query';
 import { getQueryKey } from '@trpc/react-query';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 import { Platform } from 'react-native';
 import { useError } from './useError';
 import { useIsSubscribeWithoutAuth } from './useIsSubscribeWithoutAuth';
@@ -48,12 +50,28 @@ export const useToggleSubscribeWithoutAuth = () => {
 
   const toggleSubscribeWithoutAuth = async () => {
     if (!token) {
+      setError("Il semble qu'il y ait un problème avec votre appareil, veuillez réessayer ultérieurement");
       return;
     }
     if (isSubscribed) {
       await unsubscribeFromChabanWithoutAuth({ token });
     } else {
-      await subscribeToChabanWithoutAuth({ token, os: Platform.OS === 'android' ? 'ANDROID' : 'IOS' });
+      if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+          const { status } = await Notifications.requestPermissionsAsync();
+          finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+          setError('Vous devez autoriser les notifications pour vous inscrire');
+          return;
+        }
+        await subscribeToChabanWithoutAuth({ token, os: Platform.OS === 'android' ? 'ANDROID' : 'IOS' });
+      } else {
+        setError('Vous devez être sur un appareil pour vous inscrire');
+        return;
+      }
     }
   };
   return { toggleSubscribeWithoutAuth, isToggleLoading: isLoading };
