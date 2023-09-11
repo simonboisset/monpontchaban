49b574dd9ff51b81bddf105be8135466bd02397b
Simon Boisset <simon@lezo.dev>
2023-09-12 00:23:09 +0200

    feat add Facebook link

--- apps/chaban-web/app/components/Header.tsx
+++ apps/chaban-web/app/components/Header.tsx
@@ -1,4 +1,6 @@
 import { fr } from '@lezo-alert/chaban-core';
+import { Link } from '@remix-run/react';
+import { Facebook } from 'lucide-react';
 import { Theme } from '~/hooks/useDarkMode';
 import { Moon } from './Moon';
 import { Sun } from './Sun';
@@ -7,6 +9,12 @@ export const Header = ({ theme, toggleTheme }: { toggleTheme: () => void; theme:
   return (
     <header className='fixed w-full flex flex-row top-0 left-0 right-0 p-4 gap-4 items-center text-lg sm:text-3xl backdrop-blur-lg z-30 bg-gray-800 bg-opacity-20'>
       <h1 className='grow flex'>{fr.MyChaban}</h1>
+      <Link
+        aria-label='Facebook'
+        to='https://www.facebook.com/monpontchaban'
+        className='w-6 sm:w-8 cursor-pointer h-8 sm:right-4 z-40 flex justify-center items-center bg-blue-500 rounded-lg'>
+        <Facebook />
+      </Link>
       <button className='w-6 sm:w-8 cursor-pointer h-8 sm:right-4 z-40 flex items-center' onClick={toggleTheme}>
         {theme === 'dark' ? <Moon /> : <Sun />}
       </button>
