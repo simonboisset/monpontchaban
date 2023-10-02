5ef4bfe60035e3557efd23c6618be5d057a9f525
Simon Boisset <simon@lezo.dev>
2023-10-03 00:34:39 +0200

    Update pushTokenContext.tsx

--- apps/mobile/src/services/pushTokenContext.tsx
+++ apps/mobile/src/services/pushTokenContext.tsx
@@ -1,6 +1,7 @@
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import { createContext, useContext, useEffect, useState } from 'react';
+import { Platform } from 'react-native';
 import { toast } from '../ui/useToast';
 
 type PushTokenContextType = {
@@ -32,6 +33,14 @@ export const useToken = () => {
         const { status: existingStatus } = await Notifications.getPermissionsAsync();
         let finalStatus = existingStatus;
         if (existingStatus !== 'granted') {
+          if (Platform.OS === 'android') {
+            await Notifications.setNotificationChannelAsync('default', {
+              name: 'default',
+              importance: Notifications.AndroidImportance.MAX,
+              vibrationPattern: [0, 250, 250, 250],
+              lightColor: '#FF231F7C',
+            });
+          }
           const { status } = await Notifications.requestPermissionsAsync();
           finalStatus = status;
         }
