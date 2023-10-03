069bb4bdb88cc1bbb61829004627cc3594229c9f
Simon Boisset <simon@lezo.dev>
2023-10-04 01:03:14 +0200

    feat: add new post

---
+++ apps/web/app/routes/_public.blog.$articleSlug/articles/2023-10-02-v2-notifications-personnalisees.ts
@@ -0,0 +1,60 @@
+export const notifications_personalisee = {
+  date: '2023-10-02',
+  slug: 'v2-notifications-personnalisees',
+  content: `
+  # Nouvelle Version 2.0.0 : Personnalisez Vos Alertes !
+
+  ![image](https://lezo-files.s3.fr-par.scw.cloud/chaban-blog/v2-notifications-personnalisees.webp)
+
+  *Nous sommes ravis de vous présenter la toute nouvelle version de notre application, désormais plus personnalisable. Grâce à notre dernière mise à jour, vous avez désormais le contrôle total sur les notifications que vous recevez lors des levées du Pont Chaban-Delmas.*
+  
+  ### Quoi de Neuf ?
+  
+  ## 1. Personnalisation des Notifications 📲
+  
+  Nous avons introduit une fonctionnalité qui vous permet de personnaliser vos notifications selon vos besoins. Par défaut, trois types de notifications sont créés :
+  
+  - Une heure avant la levée
+  - La veille à 20h (pour les levées du lendemain)
+  - Le dimanche à 19h (pour les levées de la semaine à venir)
+  
+  Vous pouvez ajouter, modifier ou supprimer ces notifications selon vos préférences.
+  
+  ## 2. Activation et Désactivation Faciles
+  
+  Vous pouvez activer et désactiver chaque type de notification en un simple switch. Si vous ne voulez pas être informé la veille à 20h, pas de problème, désactivez simplement cette option.
+  
+  ## 3. Personnalisation des Créneaux Horaires ⏰
+  
+  Désormais, vous pouvez choisir les créneaux horaires pendant lesquels vous souhaitez recevoir des notifications. Pour chaque type de notification, vous pouvez choisir les jours de la semaine et les heures auxquelles vous souhaitez recevoir des notifications.
+  
+  Si le pont Chaban-Delmas se lève une ou plusieurs fois entre le créneau actuel et le prochain créneau sélectionné, vous recevrez une notification listant toutes les levées prévues, comme pour la notification du dimanche à 19h qui vous informe de toutes les levées prévues pour la semaine à venir.
+  
+  Nous avons divisé la journée en créneaux horaires, chacun représentant une heure pour un jour de la semaine. Il y a 24 créneaux répartis sur 7 jours. Vous pouvez choisir les créneaux pendant lesquels vous souhaitez recevoir des notifications. 
+  
+  Par exemple, ajoutez un créneau à 12h et un à 20h chaque jour pour recevoir des notifications par demi-journée.
+  
+  ## 4. Délai de Notification 🕒
+  
+  Définissez combien de temps à l'avance vous souhaitez recevoir vos notifications. Ce délai permet de savoir sur quel créneau considérer les levées. Pour la notification du dimanche à 19h, si vous configurez votre notification pour cinq heures d'avance et que le pont se lève à 23h, vous recevrez une notification la veille à 19h et non le jour même comme si vous aviez configuré votre notification pour une heure d'avance.
+  
+  ## 5. Titre de Notification 📝
+  
+  En plus de la date et de l'heure de la levée, vous pouvez rédiger un titre personnalisé pour chaque type de notification. Par exemple, ajoutez "Le Pont Chaban se lève" ou "Le Pont Chaban se lèvera" avant la date et l'heure de la levée.
+  
+  ### Comment Personnaliser Vos Notifications ?
+  
+  1. Ouvrez l'application Mon Pont Chaban.
+  2. Allez dans les Paramètres.
+  3. Activez ou désactivez les types de notifications selon votre préférence.
+  4. Personnalisez les créneaux horaires et les délais de notification selon vos besoins.
+  
+  Voilà, c'est aussi simple que ça ! Avec ces nouvelles fonctionnalités, vous avez le contrôle total sur les horaires de fermeture du Pont Chaban-Delmas. Nous espérons que cette personnalisation rendra votre expérience avec Mon Pont Chaban encore plus pratique et agréable.
+
+  N'hésitez pas à nous faire part de vos commentaires et de vos suggestions pour continuer à améliorer Mon Pont Chaban. Nous sommes à l'écoute de votre retour pour rendre notre application encore plus utile pour tous.
+
+  Vous pouvez nous contacter par email à support@lezo.dev ou sur facebook à https://www.facebook.com/monpontchaban.
+
+  Merci de nous avoir lu et à bientôt pour de nouvelles fonctionnalités !
+  `,
+};
--- apps/web/app/routes/_public.blog.$articleSlug/articles/index.ts
+++ apps/web/app/routes/_public.blog.$articleSlug/articles/index.ts
@@ -1,7 +1,9 @@
 import { les_premiers_pas } from './2023-09-20-les-premiers-pas';
 import { recap_hebdomadaire } from './2023-09-25-recap-hebdomadaire';
+import { notifications_personalisee } from './2023-10-02-v2-notifications-personnalisees';
 
 export const articles = {
   [les_premiers_pas.slug]: les_premiers_pas,
   [recap_hebdomadaire.slug]: recap_hebdomadaire,
+  [notifications_personalisee.slug]: notifications_personalisee,
 };
