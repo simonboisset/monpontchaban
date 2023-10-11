378672a3a1c912b7ca3c453dc0714fccfe7b472d
Simon Boisset <simon@lezo.dev>
2023-10-11 10:17:12 +0200

    fix: same week on sunday

--- packages/chaban-core/src/utils/date.ts
+++ packages/chaban-core/src/utils/date.ts
@@ -2,6 +2,6 @@ import dayjs from 'dayjs';
 
 export const isToday = (date: Date) => dayjs(date).isSame(dayjs(), 'day');
 export const isTomorrow = (date: Date) => dayjs(date).isSame(dayjs().add(1, 'day'), 'day');
-export const isThisWeek = (date: Date) => dayjs(date).subtract(1, 'day').isSame(dayjs().subtract(1, 'day'), 'week');
+export const isThisWeek = (date: Date) => dayjs(date).subtract(2, 'day').isSame(dayjs().subtract(2, 'day'), 'week');
 export const isNextWeek = (date: Date) =>
-  dayjs(date).subtract(1, 'day').isSame(dayjs().subtract(1, 'day').add(1, 'week'), 'week');
+  dayjs(date).subtract(2, 'day').isSame(dayjs().subtract(2, 'day').add(1, 'week'), 'week');
